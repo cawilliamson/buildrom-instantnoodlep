ac885859fa92840ddfb32a0e97ea90b7defed258
Christopher A. Williamson <home@chrisaw.com>
2020-07-12 18:52:19 +0100

    Add HavocOS

--- deps/instantnoodlep/derpfest/build.sh
+++ deps/instantnoodlep/derpfest/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# hack to fix build error (missing smomo_interface.h)
+rm -rf frameworks/native
+git clone --depth=1 https://github.com/DerpFest-Sanders/platform_frameworks_native.git -b ten frameworks/native
+
 lunch derp_instantnoodlep-userdebug
 mka kronic -j"$(nproc --all)"
---
+++ deps/instantnoodlep/havoc/repos.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ROM_REPO="https://github.com/Havoc-OS/android-manifest.git"
+ROM_BRANCH="ten"
+export ROM_REPO ROM_BRANCH
