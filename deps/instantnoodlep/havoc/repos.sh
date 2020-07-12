c13ff987c1913d1b06766341a0490e0feb63f60f
Christopher A. Williamson <home@chrisaw.com>
2020-07-12 19:00:07 +0100

    Fix havoc repo url

--- deps/instantnoodlep/havoc/repos.sh
+++ deps/instantnoodlep/havoc/repos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ROM_REPO="https://github.com/Havoc-OS/android-manifest.git"
+ROM_REPO="https://github.com/Havoc-OS/android_manifest.git"
 ROM_BRANCH="ten"
 export ROM_REPO ROM_BRANCH
