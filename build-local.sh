9cbccb813fe1cd13645fb69b0b456dcfcb67bb32
Christopher A. Williamson <home@chrisaw.com>
2020-07-12 14:09:53 +0100

    Fix duplicate entry issue

--- build-local.sh
+++ build-local.sh
@@ -27,7 +27,7 @@ docker build -t chrisawcom/buildrom .
 
 # docker run
 echo "[*] Starting ROM build"
-docker run --user $(id -u):$(id -g) --privileged --tty --rm -e DEVICE="${1}" -e ROM="${2}" -v "$(pwd)/src:/usr/src/rom" -v "$(pwd):/var/tmp/buildrom" -v "$(pwd)/tmp/${1}/${2}:/var/tmp/buildrom/out" chrisawcom/buildrom
+docker run --privileged --tty --rm -e DEVICE="${1}" -e ROM="${2}" -v "$(pwd)/src:/usr/src/rom" -v "$(pwd):/var/tmp/buildrom" -v "$(pwd)/tmp/${1}/${2}:/var/tmp/buildrom/out" chrisawcom/buildrom
 
 # mount gdrive
 #echo "[*] Mounting Google Drive"
